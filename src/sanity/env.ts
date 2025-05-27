1290cf862879d7b9a60a71994d97f8d61587b78c
Jean-Charles Bouchaud <jcbouchaud.sh@gmail.com>
2025-05-28 00:28:29 +0200

    fix env

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,13 +1,17 @@
-export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2025-05-27";
+export const apiVersion =
+  process.env.SANITY_STUDIO_API_VERSION ||
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ||
+  "2025-05-27";
 
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET,
-  "Missing environment variable: SANITY_STUDIO_DATASET"
+  process.env.SANITY_STUDIO_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: SANITY_STUDIO_DATASET or NEXT_PUBLIC_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID,
-  "Missing environment variable: SANITY_STUDIO_PROJECT_ID"
+  process.env.SANITY_STUDIO_PROJECT_ID ||
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: SANITY_STUDIO_PROJECT_ID or NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
 export function assertValue<T>(v: T | undefined, errorMessage: string): T {
