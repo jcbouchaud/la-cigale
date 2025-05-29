d5e9e66434976df0e91ccb6bdac5a5c07e351579
Jean-Charles Bouchaud <jcbouchaud.sh@gmail.com>
2025-05-29 10:08:17 +0200

    titre mentions legales

--- src/app/(main)/mentions-legales/page.tsx
+++ src/app/(main)/mentions-legales/page.tsx
@@ -1,7 +1,9 @@
+import { VStack } from "@/components/ui/vstack";
 import { sanityFetch } from "@/sanity/lib/live";
 import { RESTAURANT_LEGAL_NOTICE_QUERY } from "@/sanity/lib/queries";
 import { PortableText } from "next-sanity";
 import { notFound } from "next/navigation";
+import { Text } from "@/components/ui/text";
 export default async function LegalNoticePage() {
   const { data } = await sanityFetch({
     query: RESTAURANT_LEGAL_NOTICE_QUERY,
@@ -14,7 +16,14 @@ export default async function LegalNoticePage() {
 
   return (
     <section className="p-8">
-      <PortableText value={data.legal_notice ?? []} />
+      <VStack className="items-center">
+        <VStack className="container gap-4">
+          <Text as="h1" variant="title">
+            Mentions légales
+          </Text>
+          <PortableText value={data.legal_notice ?? []} />
+        </VStack>
+      </VStack>
     </section>
   );
 }
